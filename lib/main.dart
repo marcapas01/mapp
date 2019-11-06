864c79a981e03bf5c403a8571c56e37375d7ed38
Marc Neil Apas <marcapas01@gmail.com>
2019-11-07 01:59:54 +0800

    App Function: Press button, map camera view will move to current position of device using geolocator plugin
    AndroidX migration did not work.

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State<MyApp> {
   }
 
 }*/
-
+/*
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -166,6 +166,70 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}*/
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final Map<String, Marker> _markers = {};
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void _getLocation() async {
+    var currentLocation = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    setState(() {
+      _markers.clear();
+      final marker = Marker(
+        markerId: MarkerId("curr_loc"),
+        position: LatLng(currentLocation.latitude, currentLocation.longitude),
+        infoWindow: InfoWindow(title: 'Your Location'),
+      );
+      _markers["Current Location"] = marker;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getLocation,
+          tooltip: 'Get Location',
+          child: Icon(Icons.flag),
         ),
       ),
     );
